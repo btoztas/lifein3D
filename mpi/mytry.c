984c9998cb0e57a39609fc30bdc37f7a9d59e208
pedromartinho <pedro.martinho.uniplaces@gmail.com>
2017-05-15 19:33:20 +0100

    mytry 7

--- mpi/mytry.c
+++ mpi/mytry.c
@@ -21,6 +21,7 @@ int main (int argc, char *argv[]) {
   MPI_Comm_rank (MPI_COMM_WORLD, &id);
   MPI_Comm_size (MPI_COMM_WORLD, &p);
 
+  rounds=atoi(argv[1]);
 
   MPI_Barrier (MPI_COMM_WORLD);
   secs = - MPI_Wtime();
