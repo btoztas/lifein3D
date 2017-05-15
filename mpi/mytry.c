4f812a4e9f23e14826df78926a1c574551b0a44f
pedromartinho <pedro.martinho.uniplaces@gmail.com>
2017-05-15 18:51:15 +0100

    mytry 3

--- mpi/mytry.c
+++ mpi/mytry.c
@@ -14,31 +14,27 @@ int main (int argc, char *argv[]) {
   MPI_Status status;
   int id, p, i, rounds;
   double secs;
-  int eu1[5]={1,2,3,4,5};
-  int eu2[5]={6,7,8,9,10};
+  int eu[5];
   int *tu;
 
-  MPI_Init (&argc, &argv);
+  MPI_Init (&argc, &argv, &eu);
   MPI_Comm_rank (MPI_COMM_WORLD, &id);
   MPI_Comm_size (MPI_COMM_WORLD, &p);
 
-  if(argc != 2){
-  	if (!id) printf ("Command line: %s <n-rounds>\n", argv[0]);
-  	MPI_Finalize();
-  	exit(1);
-  }
   rounds = atoi(argv[1]);
 
   MPI_Barrier (MPI_COMM_WORLD);
   secs = - MPI_Wtime();
 
   if(id ==0){
-    MPI_Send(eu1, 5, MPI_INT, 1, 1, MPI_COMM_WORLD);
-    MPI_Recv(tu, 5, MPI_INT, 1, 2, MPI_COMM_WORLD, &status);
+    int eu={1,2,3,4,5};
+    MPI_Send(&eu1, 5, MPI_INT, 1, 1, MPI_COMM_WORLD);
+    MPI_Recv(&tu, 5, MPI_INT, 1, 2, MPI_COMM_WORLD, &status);
   }
   if(id ==1){
-    MPI_Send(eu2, 5, MPI_INT, 0, 2, MPI_COMM_WORLD);
-    MPI_Recv(tu, 5, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+
+    MPI_Send(&eu2, 5, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Recv(&tu, 5, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
   }
 
   MPI_Barrier (MPI_COMM_WORLD);
