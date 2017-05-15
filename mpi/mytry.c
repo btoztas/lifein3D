3168ac841eabcbc3c2da66aa69b6426a7d22639f
pedromartinho <pedro.martinho.uniplaces@gmail.com>
2017-05-15 19:07:54 +0100

    mytry 7

--- mpi/mytry.c
+++ mpi/mytry.c
@@ -15,7 +15,7 @@ int main (int argc, char *argv[]) {
   int id, p, i, rounds;
   double secs;
   int eu[5];
-  int *tu;
+  int tu[5];
 
   MPI_Init (&argc, &argv);
   MPI_Comm_rank (MPI_COMM_WORLD, &id);
